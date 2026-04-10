bb39795fcd89777e17c6cc142a25338f2cd483fa
김영준 <bill5500@naver.com>
2026-04-10 15:13:09 +0900

    chore: spring boot 3.2 project scaffolding with gradle wrapper

---
+++ backend/src/main/java/com/bizmap/BizmapApplication.java
@@ -0,0 +1,11 @@
+package com.bizmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizmapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BizmapApplication.class, args);
+    }
+}
